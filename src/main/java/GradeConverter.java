495e619676da9a7069e6e3f084c6967eae966ab1
nthnhu26 <nguyennhu3570@gmail.com>
2024-05-07 15:46:12 +0700

    Test Code for Jenkins

--- src/main/java/GradeConverter.java
+++ src/main/java/GradeConverter.java
@@ -2,16 +2,16 @@ public class GradeConverter {
     public String convert(int score) {
         if(score <=100 && score >90){
             return "A";
-        } else if(score <=89 && score >80){
+        } else if(score <=90 && score >80){
             return "B";
         }
-        else if(score <=79 && score >70){
+        else if(score <=80 && score >70){
             return "C";
         }
-        else if(score <=69 && score >60){
+        else if(score <=70 && score >60){
             return "D";
         }
-        else if(score <=59 && score >0){
+        else if(score <=60 && score >0){
             return "E";
         }
         else {
